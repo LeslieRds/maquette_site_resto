19204d946c71f78abc32cf9d186b84e1fb7e74c7
Leslie Rados <leslie.rados@gmail.com>
2023-11-27 16:38:12 +0100

    ..

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,45 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update destroy]
+
+  def index
+    @articles = Article.all.order(created_at: :desc)
+  end
+
+  def show; end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    article = Article.create(article_params)
+    redirect_to article_path(article)
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    @article.update(article_params)
+    redirect_to article_path(@article), status: :see_other
+  end
+
+  def destroy
+    Article.find(params[:id]).destroy
+    redirect_to articles_path, status: :see_other
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,3 @@
 class Article < ApplicationRecord
-  belongs_to :user
-
-  validates :title, presence: true
-  validates :description, presence: true
 
 end
